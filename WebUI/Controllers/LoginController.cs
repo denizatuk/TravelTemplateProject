844f685200ee9da7c85180b0288081a741368854
denizatuk <dnz.atk96@gmail.com>
2022-08-24 17:59:57 +0300

    51

---
+++ WebUI/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
